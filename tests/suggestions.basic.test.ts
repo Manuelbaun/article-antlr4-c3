4227f888e8e22f03f80f76b432327714430cd3ae
Manuel Baun <manuel.baun@gmail.com>
2022-07-25 22:57:22 +0200

    update dependencies

--- tests/suggestions.basic.test.ts
+++ tests/suggestions.basic.test.ts
@@ -1,34 +1,46 @@
-import {expect} from "chai";
+import { expect } from "chai";
 
-import { computeTokenPosition, getSuggestions, setTokenMatcher, filterTokens, filterTokens_fuzzySearch } from "../src";
+import {
+  computeTokenPosition,
+  getSuggestions,
+  setTokenMatcher,
+  filterTokens,
+  filterTokens_fuzzySearch,
+} from "../src";
 
-const suite = function() {
-    it("are suggested",
-        function() {
-            const code = `fun test() {
+const suite = function () {
+  it("are suggested", async function () {
+    const code = `fun test() {
     try {
         doSomething()
     } 
 }`;
-            let suggestions = getSuggestions(code, { line: 4, column: 7 }, computeTokenPosition);
-            expect(suggestions.length).to.equal(51);
-        });
-    it("are suggested with partial match",
-        function() {
-            const code = `fun test() {
+    let suggestions = await getSuggestions(
+      code,
+      { line: 4, column: 7 },
+      computeTokenPosition
+    );
+    expect(suggestions.length).to.equal(51);
+  });
+  it("are suggested with partial match", async function () {
+    const code = `fun test() {
     try {
         doSomething()
     } ca
 }`;
-            let suggestions = getSuggestions(code, { line: 4, column: 8 }, computeTokenPosition);
-            expect(suggestions.indexOf('catch')).to.be.greaterThan(-1);
-        });
+    let suggestions = (await getSuggestions(
+      code,
+      { line: 4, column: 8 },
+      computeTokenPosition
+    )) as string[];
+    expect(suggestions.indexOf("catch")).to.be.greaterThan(-1);
+  });
 };
 
-describe('Keywords', suite);
-describe('Keywords w/fuzzy completion', function() {
-    let oldMatcher = filterTokens;
-    beforeEach(() => setTokenMatcher(filterTokens_fuzzySearch));
-    suite();
-    afterEach(() => setTokenMatcher(oldMatcher));
+describe("Keywords", suite);
+describe("Keywords w/fuzzy completion", function () {
+  let oldMatcher = filterTokens;
+  beforeEach(() => setTokenMatcher(filterTokens_fuzzySearch));
+  suite();
+  afterEach(() => setTokenMatcher(oldMatcher));
 });
